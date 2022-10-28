9a408b2027d61c281eee906073d626cba36007e0
hdeng <hdeng10@slb.com>
2022-10-28 14:46:52 +0800

    feat:return_2A0B_When_given_the_guess_digits_having_2_correct_with_secret_number

--- BullsAndCows/BullsAndCowsGame.cs
+++ BullsAndCows/BullsAndCowsGame.cs
@@ -5,17 +5,34 @@ namespace BullsAndCows
     public class BullsAndCowsGame
     {
         private readonly SecretGenerator secretGenerator;
+        private string secret;
         public BullsAndCowsGame(SecretGenerator secretGenerator)
         {
             this.secretGenerator = secretGenerator;
+            secret = secretGenerator.GenerateSecret();
         }
 
         public bool CanContinue => true;
 
         public string Guess(string guess)
         {
-            if guess
-            return "4A0B";
+            return CountBulls(guess);
+        }
+
+        private string CountBulls(string guess)
+        {
+            var guessDigits = guess.Split(' ');
+            var secretDigits = secret.Split(' ');
+            int countBulls = 0;
+            for (var index = 0; index < secretDigits.Length; index++)
+            {
+                if (guessDigits[index] == secretDigits[index])
+                {
+                    countBulls++;
+                }
+            }
+
+            return $"{countBulls}A0B";
         }
     }
 }
--- BullsAndCowsTest/BullsAndCowsGameTest.cs
+++ BullsAndCowsTest/BullsAndCowsGameTest.cs
@@ -16,13 +16,23 @@ namespace BullsAndCowsTest
         }
 
         [Fact]
-        public void Should_return_4A0B_When_given_the_digits_are_same_with_secret_number()
+        public void Should_return_4A0B_When_given_the_guess_digits_are_same_with_secret_number()
         {
             var mocksecretGenerator = new Mock<SecretGenerator>();
             mocksecretGenerator.Setup(generator => generator.GenerateSecret()).Returns("1 2 3 4");
             var game = new BullsAndCowsGame(mocksecretGenerator.Object);
             var guessResult = game.Guess("1 2 3 4");
             Assert.Equal("4A0B", guessResult);
+        }
+
+        [Fact]
+        public void Should_return_2A0B_When_given_the_guess_digits_having_2_correct_with_secret_number()
+        {
+            var mocksecretGenerator = new Mock<SecretGenerator>();
+            mocksecretGenerator.Setup(generator => generator.GenerateSecret()).Returns("1 2 3 4");
+            var game = new BullsAndCowsGame(mocksecretGenerator.Object);
+            var guessResult = game.Guess("1 2 5 7");
+            Assert.Equal("2A0B", guessResult);
         }
     }
 }
